bf42fbc2653fc364ba8b69035e3ff3eb0dd557b4
sentrip <djordje.m.pepic@gmail.com>
2018-07-10 02:08:28 +0200

    Added app context managers

--- src/contextlib.lua
+++ src/contextlib.lua
@@ -3,74 +3,20 @@
 
 require("src/logic")
 
---- Exception object
--- @type Exception
-Exception = class('Exception')
-
---- PlaceHolder
--- @param _type
--- @param message
-function Exception:__init(_type, message)
-  self.type = _type
-  self.message = message or ''
-end
-
-function Exception:__tostring()
-  return Exception.add_traceback('<'..self.type..'> '..self.message)
-end
-
-function Exception:__repr()
-  return tostring(self)
-end
-
-
-function Exception:__call(message)
-  return Exception(self.type, message)
-end
-
----- Placeholder
--- @param s
-function Exception.add_traceback(s, force)
-  local start = list()
-  local traceback = debug.traceback()
-  if traceback then 
-    local lines = traceback:split('\n')
-    local count = 0
-    for i, ln in pairs(lines) do
-      if ln:startswith('\t[C]') then 
-        count = count + 1 
-        start:append(i)
-      end 
-    end
-    if not lines:contains("\t[C]: in function 'error'") and not force then return s end
-    if not force then lines = lines(start[-2]) else lines = lines(2) end
-    s = s..'\nstack traceback:\n'..table.concat(lines, '\n') 
-  end
-  return s
-end
-
---Some useful exceptions
-AssertionError = Exception('AssertionError')
-IOError = Exception('IOError')
-KeyError = Exception('KeyError')
-OSError = Exception('OSError')
-TypeError = Exception('TypeError')
-ValueError = Exception('ValueError')
-
-
----- Placeholder
+---- Safely execute a function
 -- @param f 
--- @param except 
+-- @param _except 
 -- @param finally
-function try(f, except, finally)
-  except = except or function() end
-  local success, result = xpcall(f, except)
+function try(f, _except, finally)
+  _except = _except or function() end
+  local success, result = xpcall(f, _except)
   if finally then finally() end
   if not success and result then error(tostring(result)) end
   return result
 end
 
----- Placeholder
+
+---- Handle exceptions of specific types
 -- @param types
 -- @param f
 function except(types, f)
@@ -110,6 +56,100 @@ function except(types, f)
   end
 end
 
+
+---- Execute a function within a context
+-- @param context
+-- @param _do
+function with(context, _do)
+  local ctx = context()
+  local success, result = coroutine.resume(ctx)
+  if success then
+    local _type, e
+    try(
+      function() _do(result) end,
+      except(function(err) 
+          e = err
+          if err.type then _type, e = err.type, err.msg end
+        end),
+      function() context:__exit(_type, e) end
+    )
+  end
+  coroutine.resume(ctx)
+end
+
+
+---- Yield control back to function inside the current context
+-- @param ...
+function yield(...) coroutine.yield(...) end
+
+
+---- Create a ContextManager from a function
+-- @param f
+function contextmanager(f)
+  return function(...)
+    local Context = ContextManager(...)
+    Context.__enter = function(self)
+      return f(unpack(self.args))
+    end
+    return Context
+  end
+end
+
+
+---- Open a file with a given mode, ensuring to close it after
+-- @param name
+-- @param mode
+function open(name, mode)
+  if rootDir then name = pathJoin(rootDir(), name) end
+  local f = assert(io.open(name, mode or 'r'))
+  yield(f)
+  f:close()
+end
+
+
+
+---- Run a clean copy of an app and close it after
+-- @param name 
+-- @param close_after
+function run_and_close(name, close_after)
+  if close_after ~= false then close_after = true end
+  if appState(name) ~= "NOT RUNNING" then appKill(name) end
+  appRun(name)
+  yield()
+  if close_after then appKill(name) end
+end
+---- Run an app and close it after if it is not already running
+-- @param name 
+function run_if_closed(name)
+  local run_kill = false
+  if appState(name) ~= "ACTIVE" then 
+    run_kill = true 
+  end
+
+  if run_kill then appRun(name) end
+  yield()
+  if run_kill then appKill(name) end
+
+end
+
+
+
+---- Suppress exceptions of a given type
+-- @param ...
+function suppress(...)
+  local errors = {... or '.*'}
+  local Context = ContextManager()
+  
+  Context.__exit = function(self, _type, value)
+    local e = except(errors)(_type or value)
+    if e then error(e) end
+  end
+  
+  return Context
+end
+
+
+
 ------ Context manager object
 ---- @type ContextManager
 ContextManager = class("ContextManager")
@@ -132,13 +172,13 @@ function ContextManager:__call()
   end)
 end
 
----- Placeholder
+---- Enter (setup) the context
 -- @param ...
 function ContextManager:__enter()
   return self 
 end
 
----- Placeholder
+---- Exit (teardown) the context
 -- @param _type
 -- @param value
 function ContextManager:__exit(_type, value) 
@@ -151,70 +191,64 @@ function ContextManager:__exit(_type, value)
     error(err)
   end
 end
+  
 
----- Placeholder
--- @param context
--- @param _do
-function with(context, _do)
-  local ctx = context()
-  local success, result = coroutine.resume(ctx)
-  if success then
-    local _type, e
-    try(
-      function() _do(result) end,
-      except(function(err) 
-          e = err
-          if err.type then _type, e = err.type, err.msg end
-        end),
-      function() context:__exit(_type, e) end
-    )
-  end
-  coroutine.resume(ctx)
+--- Exception object
+-- @type Exception
+Exception = class('Exception')
+
+--- Create an Exception object
+-- @param _type
+-- @param message
+function Exception:__init(_type, message)
+  self.type = _type
+  self.message = message or ''
+end
+
+function Exception:__tostring()
+  return Exception.add_traceback('<'..self.type..'> '..self.message)
+end
+
+function Exception:__repr()
+  return tostring(self)
 end
 
 
----- Placeholder
--- @param f
-function contextmanager(f)
-  return function(...)
-    local Context = ContextManager(...)
-    Context.__enter = function(self)
-      return f(unpack(self.args))
+function Exception:__call(message)
+  return Exception(self.type, message)
+end
+
+---- Recent traceback for error tracking
+-- @param s
+-- @param force
+function Exception.add_traceback(s, force)
+  local start = list()
+  local traceback = debug.traceback()
+  if traceback then 
+    local lines = traceback:split('\n')
+    local count = 0
+    for i, ln in pairs(lines) do
+      if ln:startswith('\t[C]') then 
+        count = count + 1 
+        start:append(i)
+      end 
     end
-    return Context
+    if not lines:contains("\t[C]: in function 'error'") and not force then return s end
+    if not force then lines = lines(start[-2]) else lines = lines(2) end
+    s = s..'\nstack traceback:\n'..table.concat(lines, '\n') 
   end
-end
-
----- Placeholder
--- @param ...
-function yield(...) coroutine.yield(...) end
-
-
-
----- Placeholder
--- @param name
--- @param mode
-function open(name, mode)
-  if rootDir then name = pathJoin(rootDir(), name) end
-  local f = assert(io.open(name, mode or 'r'))
-  yield(f)
-  assert(f:close())
+  return s
 end
 
 
----- Placeholder
--- @param ...
-function suppress(...)
-  local errors = {... or '.*'}
-  local Context = ContextManager()
-  
-  Context.__exit = function(self, _type, value)
-    local e = except(errors)(_type or value)
-    if e then error(e) end
-  end
-  
-  return Context
-end
-  
+--Some useful exceptions
+AssertionError = Exception('AssertionError')
+IOError = Exception('IOError')
+KeyError = Exception('KeyError')
+OSError = Exception('OSError')
+TypeError = Exception('TypeError')
+ValueError = Exception('ValueError')
+
 --Context manager function wrapping, see contextmanager function for details
 open = contextmanager(open)
+run_if_closed = contextmanager(run_if_closed)
