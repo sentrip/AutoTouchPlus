0f0eaf55c5a76a5dcc01fe935dac3a58da82bbb5
sentrip <djordje.m.pepic@gmail.com>
2018-10-18 03:11:12 +0200

    Added test case for string slicing

--- tests/test_string.lua
+++ tests/test_string.lua
@@ -8,14 +8,17 @@ require('src/system')
 
 
 describe('string', 
+  
   it('startswith', function() 
      assert(('\nabc'):startswith('\n'), 'Startswith \\n')
      assert(not ('abc'):startswith('\n'), 'Not Startswith \\n')
-    end),
+  end),
+
   it('endswith', function() 
     assert(('abc\n'):endswith('\n'), 'Endswith \\n')
     assert(not ('abc'):endswith('\n'), 'Not endswith \\n')
-    end),
+  end),
+
   it('format', function() 
     local o = 'Hello {}, {} and {}'
     local x = 'Hello {:-8}, {:2}, {:3}'
@@ -27,16 +30,19 @@ describe('string',
     assertEqual(x:format('k', 'bob', 1000), ex, 'Bad string format')
     assertEqual(y:format{j='john', d='djordje'}, ey, 'Bad string format')
     assertEqual(('%s, %d'):format('hello', 100), 'hello, 100', 'Bad string format')
-    end),
+  end),
+
   it('join', function() 
     assertEqual(('\n'):join({'a', 'b'}), 'a\nb', 
       'String join table failed')
     assertEqual(('\n'):join(list{'a', 'b'}), 'a\nb', 
       'String join list failed')
-    end),
+  end),
+
   it('replace', function()  
     assertEqual(('abcba'):replace('b', 'q'), 'aqcqa', 'String replacement incorrect') 
-    end),
+  end),
+
   it('split', function()
     local s1 = 'abc'
     local s2 = 'aabbcc'
@@ -45,18 +51,22 @@ describe('string',
     assertRequal(s1:split('b'), {'a', 'c'}, 'Split with char arg')
     assertRequal(s2:split('bb'), {'aa', 'cc'}, 'Split with string arg')
     assertRequal(s3:split(' '), {'a', '20', '300', 'dddd'}, 'Split with spaces')
-    end),
+  end),
+
   it('strip', function()
     local x = 'abacbabacabab'
     local expected = 'cbabac'
     assertEqual(x:strip('ab'), expected, 'String not stripped correctly')
-    end),
+  end),
+
   it('str_add', function() 
     assertEqual('a' + 'b', 'ab', 'String add incorrect')
-    end),
+  end),
+
   it('str_mul', function() 
     assertEqual('ab' * 2, 'abab', 'String mul incorrect') 
-    end),
+  end),
+
   it('str_pairs', function() 
     local c = 1
     local _t = {'a', 'b', 'c'}
@@ -65,7 +75,8 @@ describe('string',
       assertEqual(v, _t[c], 'String pairs value incorrect')
       c = c + 1 
     end
-    end),
+  end),
+
   it('str_index', function() 
     local s = 'abc'
     for i, v in pairs(string) do
@@ -73,7 +84,10 @@ describe('string',
     end
     assertEqual(s[1], 'a', 'Positive string index failed')
     assertEqual(s[-1], 'c', 'Negative string index failed')
-    end)
+    local x = 'abcde'
+    assert(x(2, 4) == 'bcd', 'Did not slice string correctly')
+    assert(x{1, -2, 3} == 'adc', 'Did not slice string correctly')
+  end)
 )
 
 
