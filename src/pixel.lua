c37f3b7d3c41285873fd333a4e20132d8378ee7e
sentrip <djordje.m.pepic@gmail.com>
2018-10-19 05:08:22 +0200

    Added pixel regions and tests for pixel

--- src/pixel.lua
+++ src/pixel.lua
@@ -1,39 +1,66 @@
---- Pixel objects for screen manipulation
+--- Pixel objects for screen watching
 -- @module pixel
 
 --Basic colors
-BLACK = 0
-WHITE = 16777215
+colors = {
+  aqua    = 65535,
+  black   = 0,
+  blue    = 255,
+  fuchsia = 16711935,
+  gray    = 8421504,
+  green   = 32768,
+  lime    = 65280,
+  maroon  = 8388608,
+  navy    = 128,
+  olive   = 8421376,
+  orange  = 16753920,
+  purple  = 8388736,
+  red     = 16711680,
+  silver  = 12632256,
+  teal    = 32896,
+  yellow  = 16776960,
+  white   = 16777215
+}
+
+
 
 ---- Pixel object
 -- @type Pixel
 Pixel = class('Pixel')
-
 function Pixel:__init(x, y, color)
   self.x = x
   self.y = y
-  self.expected_color = color or WHITE
+  self.expected_color = color or colors.white
 end
+
 function Pixel:__add(position)
-  self.x = self.x + (position.x or position[1] or 0)
-  self.y = self.y + (position.y or position[1] or 0)
+  local x = self.x + (position.x or position[1] or 0)
+  local y = self.y + (position.y or position[2] or 0)
+  return Pixel(x, y, self.expected_color)
 end
 
 function Pixel:__sub(position)
-  self.x = self.x - (position.x or position[1] or 0)
-  self.y = self.y - (position.y or position[1] or 0)
+  local x = self.x - (position.x or position[1] or 0)
+  local y = self.y - (position.y or position[2] or 0)
+  return Pixel(x, y, self.expected_color)
 end
 
 function Pixel:__eq(pixel)
-  return self.x == pixel.x and self.y == pixel.y and self.color == pixel.color
+  return self.x == pixel.x and self.y == pixel.y and self.expected_color == pixel.expected_color
 end
----
+
+function Pixel:__hash()
+  return self.x * 1000000000000 + self.y * 100000000 + self.expected_color
+end
+
+function Pixel:__tostring()
+  return string.format('<Pixel(%d, %d)>', self.x, self.y)
+end
 
 ---
-Pixel.__getters['color'] = function(self)
+property(Pixel, 'color', function(self)
   return getColor(self.x, self.y)
-end
----
+end)
 
 ---- Returns a function that checks if the pixel's color has changed
 -- @treturn function check whether the color has changed
@@ -48,85 +75,88 @@ function Pixel:color_changed()
     return false
   end
 end
----
 
 ---- Check if the pixel is in a screen
 -- @treturn boolean is the pixel visible on the screen
 function Pixel:visible()
   return self.color == self.expected_color
 end
----
+
+
 
 ---- Collection of Pixel objects
 -- @type Pixels
 Pixels = class('Pixels')
-
 function Pixels:__init(pixels)
   self.pixels = list()
   self.expected_colors = list()
-  --table of pixels
-  if is(pixels) and getmetatable(pixels[1]) then
-    self.pixels = pixels    
-    for i, pixel in pairs(pixels) do
-      self.expected_colors:append(pixel.color)
+  local pixel_set = set()
+  for i, pixel in pairs(pixels or {}) do
+    if not isinstance(pixel, Pixel) then
+      pixel = Pixel(pixel[1], pixel[2], pixel[3])
     end
-  --table of tables {x, y, color}
-  else
-    for i, t in pairs(pixels) do
-      self.pixels:append(Pixel{t[1], t[2], t[3]})
-      self.expected_colors:append(t[3] or WHITE)
+    if not pixel_set:contains(pixel) then
+      pixel_set:add(pixel)
+      self.pixels:append(pixel)
+      self.expected_colors:append(pixel.expected_color)
     end
   end
 end
 
-function Pixels:__add(pixels)
-  for i, pixel in pairs(pixels) do
-    self.pixels:append(pixel)
-    self.expected_colors:append(pixel.expected_color)
+function Pixels:__add(other)
+  local pixel_set = set(self.pixels)
+  local new_pixels = list()
+  for i, pixel in pairs(other.pixels) do
+    local pix = Pixel(pixel.x, pixel.y, pixel.expected_color)
+    if not pixel_set:contains(pix) then new_pixels:append(pix) end
   end
+  return Pixels(self.pixels + new_pixels)
 end
 
-function Pixels:__eq(pixels)
-  for i, pixel in pairs(pixels.pixels) do
+function Pixels:__sub(other)
+  local pixel_set = set(other.pixels)
+  local new_pixels = list()
+  for p in iter(self.pixels) do 
+    if not pixel_set:contains(p) then new_pixels:append(p)  end
+  end
+  return Pixels(new_pixels)
+end
+
+function Pixels:__eq(other)
+  if len(self.pixels) ~= len(other.pixels) then return false end
+  for i, pixel in pairs(other.pixels) do
     if pixel ~= self.pixels[i] then return false end
   end
   return true
 end
----
+
+function Pixels:__tostring()
+  return string.format('<Pixels(n=%d)>', len(self.pixels))
+end
 
 ---
-Pixels.__getters['positions'] = function(self)
+property(Pixels, 'colors', function(self)
   local positions = list()
-  for pixel in iter(self.pixels) do
-    positions:append({pixel.x, pixel.y})
-  end
-  return positions
-end
----
-
----
-Pixels.__getters['colors'] = function(self)
-  return getColors(self.positions)
-end
----
+  for p in iter(self.pixels) do positions:append({p.x, p.y}) end
+  return getColors(positions)
+end)
 
 ---- Check if all the pixels are in a screen
 -- @treturn boolean are all the pixels visible on the screen
 function Pixels:visible()
-  return requal(getColors(self.positions), self.expected_colors)
+  return requal(self.colors, self.expected_colors)
 end
----
 
 ---- Count how many of the pixels are in a screen
 -- @treturn number how many pixels are visible on the screen
 function Pixels:count()
+  local colors = self.colors
   local count = 0
-  for i, v in pairs(self.colors) do
+  for i, v in pairs(colors) do
     if v == self.expected_colors[i] then count = count + 1 end
   end
   return count
 end
----
 
 --- Check how many pixels have changed colors comparing to expected number
 local function n_colors_changed(pixels, n)
@@ -151,25 +181,122 @@ local function n_colors_changed(pixels, n)
   end
 end
 
----
-
 ---- Returns a function that checks if any pixels have changed color
 -- @treturn function check whether the colors have changed
 function Pixels:any_colors_changed()
   return n_colors_changed(self, 1)
 end
----
 
 ---- Returns a function that checks if all pixels have changed color
 -- @treturn function check whether the colors have changed
 function Pixels:all_colors_changed()
   return n_colors_changed(self, len(self.pixels))
 end
----
 
 ---- Returns a function that checks if n pixels have changed color
 -- @treturn function check whether the colors have changed
 function Pixels:n_colors_changed(n)
   return n_colors_changed(self, n)
 end
----
+
+
+
+---- Group of evenly spaced pixels
+-- @type Region
+Region = class('Region', 'Pixels')
+function Region:__init(positions, color)
+  self.color = color or colors.white
+  local pixels = list()
+  local pixel_set = set()
+  for p in iter(positions) do 
+    local pix = Pixel(p.x or p[1], p.y or p[2], self.color)
+    if not pixel_set:contains(pix) then 
+      pixel_set:add(pix)
+      pixels:append(pix)
+    end  
+  end
+  Pixels.__init(self, pixels)
+end
+
+function Region:__add(other)
+  assert(other:isinstance(Region), 'Can only add Region objects to other Region objects')
+  assert(other.color == self.color, 'Can only add Regions of the same color')
+  return Region(self.pixels + other.pixels, self.color)
+end
+
+function Region:__sub(other)
+  assert(other:isinstance(Region), 'Can only subtract Region objects from other Region objects')
+  assert(other.color == self.color, 'Can only subtract Regions of the same color')
+  return Region(set(self.pixels) - set(other.pixels), self.color)
+end
+
+function Region:__tostring()
+  return string.format('<Region(pixels=%d, color=%d)>', len(self.pixels), self.color)
+end
+
+property(Region, 'center', function(self)
+  local x, y = 0, 0
+  for p in iter(self.pixels) do x, y = x + p.x, y + p.y end
+  return Pixel(x / len(self.pixels), y / len(self.pixels), self.color)
+end)
+
+
+
+---- Ellipse @{Region}
+-- @type Ellipse
+Ellipse = class('Ellipse', 'Region')
+function Ellipse:__init(options)
+  self.x = options.x or 0
+  self.y = options.y or 0
+  self.width = options.width or 1
+  self.height =  options.height or 1
+  self.spacing = options.spacing or 1
+  local positions = list()
+  -- TODO: Ellipse creation
+  Region.__init(self, positions, options.color)
+end
+
+function Ellipse:__tostring()
+  return string.format('<Ellipse(%d, %d, width=%d, height=%d, spacing=%d, color=%d, pixels=%d)>', 
+  self.x, self.y, self.width, self.height, self.spacing, self.color, len(self.pixels))
+end
+
+
+
+---- Rectangle @{Region}
+-- @type Rectangle
+Rectangle = class('Rectangle', 'Region')
+function Rectangle:__init(options)
+  self.x = options.x or 0
+  self.y = options.y or 0
+  self.width = options.width or 1
+  self.height =  options.height or 1
+  self.spacing = options.spacing or 1
+  local positions = list()
+  for i=self.x, self.x + self.width, self.spacing do
+    for j=self.y, self.y + self.height, self.spacing do
+      positions:append({i, j})
+    end
+  end
+  Region.__init(self, positions, options.color)
+end
+
+function Rectangle:__tostring()
+  return string.format('<Rectangle(%d, %d, width=%d, height=%d, spacing=%d, color=%d, pixels=%d)>', 
+  self.x, self.y, self.width, self.height, self.spacing, self.color, len(self.pixels))
+end
+
+
+
+---- Triangle @{Region}
+-- @type Triangle
+Triangle = class('Triangle', 'Region')
+function Triangle:__init(options)
+  -- TODO: Triangle creation
+  local positions = list()
+  Region.__init(self, positions, options.color)
+end
+
+function Triangle:__tostring()
+  return string.format('<Triangle(n=%d, color=%d)>', len(self.pixels), self.color)
+end
--- tests/test_pixel.lua
+++ tests/test_pixel.lua
@@ -1,6 +1,233 @@
 require('src/test')
+require('src/contextlib')
+require('src/core')
+require('src/itertools')
+require('src/logic')
+require('src/objects')
 require('src/pixel')
+require('src/screen')
+require('src/string')
+require('src/system')
 
--- TODO: pixel tests
+
+fixture('pixels', function(monkeypatch) 
+  local pixels = list{
+    Pixel(10, 10, 10),
+    Pixel(20, 20, 20),
+    Pixel(30, 30, 30),
+    Pixel(40, 40, 40),
+  }
+  local function _getColor(x, y) 
+    for p in iter(pixels) do if p.x == x and p.y == y then return p.expected_color end end
+  end
+  local function _getColors(pos) 
+    local colors = list()
+    for p in iter(pos) do colors:append(_getColor(p[1], p[2])) end
+    return colors
+  end
+
+  monkeypatch.setattr('getColor', _getColor)
+  monkeypatch.setattr('getColors', _getColors)
+  return pixels
+end)
+
+
+describe('pixel - Pixel', 
+
+  it('can create and hash pixel objects', function() 
+    local pixels = set()
+    local length = 0
+    for i=1, 1000, 100 do
+      for j=1, 1000, 100 do
+        for color=1, 100000, 10000 do
+          pixels:add(Pixel(i, j, color))
+          assert(len(pixels) == length + 1, 'Did not create unique pixel')
+          length = length + 1
+        end
+      end
+    end
+  end),
+
+  it('can add and subtract pixel objects', function()
+    local pix = Pixel(10, 10, 10) 
+    local added = pix + Pixel(10, 10)
+    local subbed = pix - Pixel(10, 10)
+    assert(added:isinstance(Pixel), 'Adding pixel objects did not return pixel')
+    assert(subbed:isinstance(Pixel), 'Subtracting pixel objects did not return pixel')
+    assert(added.x == 20 and added.y == 20, 'Did not add pixel positions correctly')
+    assert(subbed.x == 0 and subbed.y == 0, 'Did not add pixel positions correctly')
+    assert(pix.expected_color == added.expected_color, 'Changed pixel color on add')
+    assert(pix.expected_color == subbed.expected_color, 'Changed pixel color on sub')
+    local added_table = pix + {10, 10}
+    local subbed_table = pix - {10, 10}
+    assert(added:isinstance(Pixel), 'Adding pixel and table did not return pixel')
+    assert(subbed:isinstance(Pixel), 'Subtracting pixel and table did not return pixel')
+    assert(added_table.x == 20 and added_table.y == 20, 'Did not add pixel positions correctly')
+    assert(subbed_table.x == 0 and subbed_table.y == 0, 'Did not add pixel positions correctly')
+    assert(pix.expected_color == added_table.expected_color, 'Changed pixel color on add with table')
+    assert(pix.expected_color == subbed_table.expected_color, 'Changed pixel color on sub with table')
+  end),
+
+  it('can evaluate pixel equality', function(pixels) 
+    assert(Pixel(10, 10, 10) == Pixel(10, 10, 10), 'Equal pixels not equal')
+    assert(Pixel(0, 10, 10) ~= Pixel(10, 10, 10), 'Non-equal pixels equal')
+    assert(Pixel(10, 0, 10) ~= Pixel(10, 10, 10), 'Non-equal pixels equal')
+    assert(Pixel(10, 10, 0) ~= Pixel(10, 10, 10), 'Non-equal pixels equal')
+  end),
+  
+  it('can stringify pixel', function() 
+    assert(tostring(Pixel(10, 10, 10)) == string.format('<Pixel(%d, %d)>', 10, 10), 'Pixel tostring not correct')
+  end),
+
+  it('can get color of pixel', function(pixels)
+    assert(Pixel(10, 10).color == 10, 'Did not get color of pixel') 
+  end),
+
+  it('can detect pixel color change', function(pixels) 
+    local pix = Pixel(10, 10)
+    local changed = pix:color_changed()
+    assert(not changed(), 'Detected change in color when there was none')
+    assert(not changed(), 'Detected change in color when there was none')
+    pixels[1].expected_color = 10000
+    assert(changed(), 'Did not detect change in pixel color')
+    assert(not changed(), 'Detected change in color when there was none')
+  end),
+
+  it('can detect pixel visibility', function(pixels) 
+    assert(Pixel(10, 10, 10):visible(), 'Visible pixel not visible') 
+    assert(not Pixel(0, 10, 10):visible(), 'Non visible pixel visible') 
+    assert(not Pixel(10, 0, 10):visible(), 'Non visible pixel visible') 
+    assert(not Pixel(10, 10, 0):visible(), 'Non visible pixel visible') 
+  end)
+)
+
+
+describe('pixel - Pixels',
+
+  it('can add and subtract Pixels objects', function() 
+    local pixels = Pixels{Pixel(0, 0), Pixel(10, 10), Pixel(10, 10)}
+    local added = pixels + Pixels{Pixel(10, 10), Pixel(20, 20)}
+    local subbed = pixels - Pixels{Pixel(10, 10), Pixel(20, 20)}
+    assert(added:isinstance(Pixels), 'Add did not return Pixels object')
+    assert(subbed:isinstance(Pixels), 'Subtract did not return Pixels object')
+    assert(len(pixels.pixels) == 2, 'Did not create pixels correctly')
+    assert(len(added.pixels) == 3, 'Did not add pixels correctly')
+    assert(len(subbed.pixels) == 1, 'Did not sub pixels correctly')
+  end),
+
+  it('can evaluate Pixels equality', function() 
+    assert(Pixels{Pixel(10, 10)} == Pixels{{10, 10}, {10, 10}}, 'Pixels are not equal')
+    assert(Pixels{Pixel(10, 10)} == Pixels{Pixel(10, 10)}, 'Equal Pixels objects are not equal')
+    assert(Pixels{Pixel(10, 10)} ~= Pixels{Pixel(10, 10), Pixel(10, 20)}, 'Not equal Pixels objects are equal')
+    assert(Pixels{Pixel(0, 10)} ~= Pixels{Pixel(10, 10)}, 'Not equal Pixels objects are equal')
+  end),
+
+  it('can stringify Pixels', function() 
+    local pixels = Pixels{Pixel(10, 10), Pixel(20, 20)}
+    assert(tostring(pixels) == string.format('<Pixels(n=%d)>', len(pixels.pixels)))
+  end),
+
+  it('can get colors of Pixels', function(pixels) 
+    local pix = Pixels{Pixel(10, 10, 10), Pixel(20, 20, 20)}
+    local expected = {pixels[1].expected_color, pixels[2].expected_color}
+    assert(requal(pix.colors, expected), 'Did not get all correct colors of Pixels')
+  end),
+
+  it('can detect Pixels visibility', function(pixels) 
+    assert(Pixels{Pixel(10, 10, 10), Pixel(20, 20, 20)}:visible(), 'Visible pixel is not visible')
+    assert(not Pixels{Pixel(10, 10, 0), Pixel(20, 20, 20)}:visible(), 'Non visible pixel is visible')
+  end),
+
+  it('can count number of different pixels', function(pixels) 
+    local pix = Pixels{Pixel(10, 10, 10), Pixel(20, 20, 20), Pixel(100, 100, 10), Pixel(200, 200, 20)}
+    assert(pix:count() == 2, 'Did not get correct pixel count')
+  end),
+
+  it('can detect specific Pixels color change configurations', function(pixels) 
+    local pix = Pixels{
+      Pixel(10, 10),
+      Pixel(20, 20),
+      Pixel(30, 30),
+      Pixel(40, 40)
+    }
+    local any_change = pix:any_colors_changed()
+    local all_change = pix:all_colors_changed()
+    local two_change = pix:n_colors_changed(2)
+    assert(not any_change(), 'Detected change when there was not one')
+    assert(not all_change(), 'Detected change when there was not one')
+    assert(not two_change(), 'Detected change when there was not one')
+    pixels[1].expected_color = 0
+    assert(any_change(), 'Did not detect change')
+    assert(not all_change(), 'Detected change when there was not one')
+    assert(not two_change(), 'Detected change when there was not one')
+    pixels[1].expected_color = 1000
+    pixels[2].expected_color = 1000
+    assert(any_change(), 'Did not detect change')
+    assert(not all_change(), 'Detected change when there was not one')
+    assert(two_change(), 'Did not detect change')
+    pixels[3].expected_color = 1000000
+    pixels[4].expected_color = 1000000
+    pixels[3].expected_color = 1000000
+    pixels[4].expected_color = 1000000
+    assert(any_change(), 'Did not detect change')
+    assert(all_change(), 'Did not detect change')
+    assert(two_change(), 'Did not detect change')
+  end)
+)
+
+-- TODO: Ellipse and Triangle tests
+
+describe('pixel - Region',
+
+  it('can add and subtract Region objects', function() 
+    local region = Region{Pixel(0, 0), Pixel(10, 10)}
+    local added = region + Region{Pixel(10, 10), Pixel(20, 20)}
+    local subbed = region - Region{Pixel(10, 10), Pixel(20, 20)}
+    assert(added:isinstance(Region), 'Add did not return Region object')
+    assert(subbed:isinstance(Region), 'Subtract did not return Region object')
+    assertRaises('Can only', function() 
+      local fail = Region{Pixel(0, 0)} + Pixels{Pixel(10, 10)}
+    end, 'Adding Region to pixels did not fail')
+    assertRaises('Can only', function() 
+      local fail = Region{Pixel(0, 0)} - Pixels{Pixel(10, 10)}
+    end, 'Subtracting Region to pixels did not fail')
+  end),
+
+  it('can stringify Region', function() 
+    local region = Region{Pixel(0, 0), Pixel(10, 10)}
+    assert(tostring(region) == string.format('<Region(pixels=%d, color=%d)>', len(region.pixels), region.color))
+  end),
+
+  it('can get center of Region', function() 
+    local region = Region{Pixel(0, 0), Pixel(10, 10)}
+    assert(region.center == Pixel(5, 5), 'Did not get correct center of region')
+  end),
+  
+  it('can create Ellipse', function() 
+  end),
+  
+  it('can create Rectangle', function() 
+    local rect = Rectangle{
+      x=10,
+      y=10,
+      width=100,
+      height=100,
+      spacing=10
+    }
+    assert(rect.x == 10, 'Rectangle has incorrect ')
+    assert(rect.y == 10, 'Rectangle has incorrect y')
+    assert(rect.width == 100, 'Rectangle has incorrect width')
+    assert(rect.height == 100, 'Rectangle has incorrect height')
+    assert(rect.spacing == 10, 'Rectangle has incorrect spacing')
+    assert(len(rect.pixels) == 121, 'Incorrect number of pixels in Rectangle')
+    assert(rect.pixels[1].x == rect.x and rect.pixels[1].y == rect.y, 'Incorrect first pixel location')
+    assert(rect.pixels[-1].x == rect.x + rect.width and rect.pixels[-1].y == rect.y + rect.height, 'Incorrect last pixel location')
+  end),
+  
+  it('can create Triangle', function() 
+  end)
+  
+)
+
 
 run_tests()
