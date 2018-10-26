0caefc2aedb37a18e1b86769800e9fd808dd4119
sentrip <djordje.m.pepic@gmail.com>
2018-10-26 15:57:14 +0200

    Moved docs to master branch

---
+++ doc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import os, sys, webbrowser
+
+
+if __name__ == '__main__':
+  os.system('cp AutoTouchPlus.lua src/AutoTouchPlus.lua')
+  os.system('ldoc src -d docs -c docs/config.ld')
+  os.system('rm  src/AutoTouchPlus.lua')
+  os.system('cp docs/modules/AutoTouchPlus.html docs/README')
+  os.system('rm  docs/modules/AutoTouchPlus.html')
+  os.system('ldoc src -d docs -c docs/config.ld')
+  with open('docs/topics/README.html') as f:
+    data = f.read().splitlines()
+  b, e, ct, ht = [], [], [], []
+  for i, ln in enumerate(data):
+    if '<!DOCTYPE html PUBLIC' in ln:
+      b.append(i)
+    if '<p><h1>Module <code>' in ln:
+      e.append(i)
+    if '</div> <!-- id="content" -->' in ln:
+      ct.append(i)
+    if '</html>' in ln:
+      ht.append(i), 
+  with open('docs/topics/README.html', 'w') as f:
+    for ln in data[:b[1]] + data[e[0]:ct[0]] + data[ht[0]:]:
+      f.write(ln + '\n')
+
+  if len(sys.argv) < 2 or sys.argv[1] != '--only-render':
+    webbrowser.open('docs/index.html')
+  
